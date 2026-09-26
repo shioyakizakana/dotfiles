5e556d63a34eedc4c4ae53daf71eeb6811e83cba
shioyakizakana <shioyakizakna@gmail.com>
2026-09-26 13:05:36 +0900

    refact(yazi):bunny-yaziの設定方法をコメントとして追記

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -13,6 +13,22 @@ require("full-border"):setup({
 })
 
 -- サードパーテイープラグイン
+-- https://github.com/tadashi-aikawa/toki/blob/7fede8c5fc086714e540004471fb2a7139444ea7/mnt/yazi/init.lua#L24
 -- `plugins/bunny-private.yazi/main.lua`を作成して設定
 -- プライベートなパスを含むため設定を分離
+-- ```
+-- local function setup()
+--  require("bunny"):setup({
+--    hops = {
+--      { key = "/", path = "/" },
+--      { key = "h" , path = "~", desc = "Home directory" },
+--      { key = "c", path = "~/.config", desc = "Config files" },
+--      { key = "d", path = "~/Downloads", desc = "Downloads"  },
+--    },
+--  })
+-- end
+--
+-- return { setup = setup }
+-- ```
 require("bunny-private"):setup()
+
